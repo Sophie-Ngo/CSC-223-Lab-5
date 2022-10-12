b6f91381f0e23eb953ec40cf98dd512d86651baa
Brycen Addison <addibr0@furman.edu>
2022-10-12 16:23:01 -0400

    UnparseVisitor now uses unique segments

--- src/input/visitor/UnparseVisitor.java
+++ src/input/visitor/UnparseVisitor.java
@@ -70,7 +70,7 @@ public class UnparseVisitor implements ComponentNodeVisitor
 		
         sb.append("    ".repeat(level)).append("{\n");
 
-        for (Entry<PointNode, Set<PointNode>> entry : node.getAdjLists().entrySet()) 
+        for (Entry<PointNode, Set<PointNode>> entry : node.uniqueEntrySet())
         {
         	PointNode a = entry.getKey();
         	sb.append("    ".repeat(level + 1)).append(a.getName()).append(" :");
