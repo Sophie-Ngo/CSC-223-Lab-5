a27f507a1b5a8e17d34f9a4f0b0642a396656556
Brycen Addison <addibr0@furman.edu>
2022-10-12 16:20:54 -0400

    feat: added GeometryBuilder

---
+++ src/input/builder/GeometryBuilder.java
@@ -0,0 +1,64 @@
+package input.builder;
+
+import input.components.FigureNode;
+import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
+import input.components.segment.SegmentNode;
+import input.components.segment.SegmentNodeDatabase;
+
+import java.util.List;
+
+/**
+ * @author brycenaddison
+ * @created Wed Oct 12 2022
+ */
+public class GeometryBuilder extends DefaultBuilder {
+    /**
+     * @param description
+     * @param points
+     * @param segments
+     * @return
+     */
+    @Override
+    public FigureNode buildFigureNode(String description, PointNodeDatabase points, SegmentNodeDatabase segments) {
+        return new FigureNode(description, points, segments);
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public SegmentNodeDatabase buildSegmentNodeDatabase() {
+        return new SegmentNodeDatabase();
+    }
+
+    /**
+     * @param pt1
+     * @param pt2
+     * @return
+     */
+    @Override
+    public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2) {
+        return new SegmentNode(pt1, pt2);
+    }
+
+    /**
+     * @param points
+     * @return
+     */
+    @Override
+    public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points) {
+        return new PointNodeDatabase(points);
+    }
+
+    /**
+     * @param name
+     * @param x
+     * @param y
+     * @return
+     */
+    @Override
+    public PointNode buildPointNode(String name, double x, double y) {
+        return new PointNode(name, x, y);
+    }
+}
